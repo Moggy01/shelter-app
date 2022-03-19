f688d7a6d355251a852bb747f61261c7c45083ea
Moggy01 <m.cawthorne@hotmail.com>
2022-03-19 15:31:28 +0000

    Nav Bar

---
+++ src/NavBarLoading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import React from 'react'
+
+export default function NavBarLoading() {
+  return (
+    <div>NavBarLoading</div>
+  )
+}
