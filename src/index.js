134b3577a7a7007a3cfa9bddeb761d1e5d2cca9c
yhemiefaj <yhemiefaj@gmail.com>
2022-03-20 11:23:03 +0000

    Fixed  provideShelterForm.jsx error

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './index.css';
-import App from './App';
-import ProvideShelterForm from './provideShelter/ProvideShelterForm';
-import RequestShelterResponse from './requestShelter/RequestShelterResponse';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import ProvideShelterForm from "./provideShelter/provideShelterForm";
+import RequestShelterResponse from "./requestShelter/RequestShelterResponse";
 
 ReactDOM.render(
   <React.StrictMode>
-  <BrowserRouter>
-  <Routes>
-    <Route path='/' element={<App/>}/>
-    {/* Host */}
-    <Route path='/provideShelter/ProvideShelterForm' element={<ProvideShelterForm/>} />
-   {/* Refugee */}
-    <Route path='./requestShelter/RequestShelterResponse' element={<RequestShelterResponse />}/> 
-  </Routes>
-  </BrowserRouter>
-</React.StrictMode>,
-document.getElementById('root')
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* Host */}
+        <Route
+          path="/provideShelter/ProvideShelterForm"
+          element={<ProvideShelterForm />}
+        />
+        {/* Refugee */}
+        <Route
+          path="./requestShelter/RequestShelterResponse"
+          element={<RequestShelterResponse />}
+        />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-
